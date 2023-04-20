b0c549c8182e96e5d0d12fe1632c47f34582bcb3
Alonsovn <alonsonh94@gmail.com>
2023-04-20 12:16:59 -0600

    numbers type

---
+++ types/numbers.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    var avengers = 50;
+    console.log(avengers);
+    var villians = 20;
+    if (avengers < villians) {
+        console.log("We are in troubles!");
+    }
+    else {
+        console.log("We are safe!");
+    }
+    console.log({ avengers: avengers });
+})();
---
+++ types/numbers.ts
@@ -0,0 +1,15 @@
+(() => {
+  let avengers = 50;
+
+  console.log(avengers);
+
+  const villians = 20;
+
+  if (avengers < villians) {
+    console.log("We are in troubles!");
+  } else {
+    console.log("We are safe!");
+  }
+
+  console.log({ avengers });
+})();
