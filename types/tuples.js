1bcae01c32b78fcb634f95f1ee7c94deba510a00
Alonsovn <alonsonh94@gmail.com>
2023-04-20 14:44:41 -0600

    tuples types

---
+++ types/tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+(function () {
+    var hero = ["Dr Strange", 100];
+    hero[0] = "Ironman";
+    hero[1] = 50;
+    console.log(hero);
+})();
---
+++ types/tuples.ts
@@ -0,0 +1,7 @@
+(() => {
+  const hero = ["Dr Strange", 100];
+  hero[0] = "Ironman";
+  hero[1] = 50;
+
+  console.log(hero);
+})();
