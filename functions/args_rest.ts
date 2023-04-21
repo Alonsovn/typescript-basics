90622d7137ae20da621b3c7c090dec6b47efc13f
Alonsovn <alonsonh94@gmail.com>
2023-04-21 13:44:49 -0600

    Add args rest..

---
+++ functions/args_rest.ts
@@ -0,0 +1,10 @@
+(() => {
+  const fullName = (firstName: string, ...args: string[]): string => {
+    return `${firstName} ${args.join(" ")}`;
+  };
+
+  const superman = fullName("Clark", "Joseph", "Kent");
+
+  console.log(superman);
+  console.log({ superman });
+})();
