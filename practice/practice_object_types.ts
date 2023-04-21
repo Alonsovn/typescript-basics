fc791cf87cd77ce8764f82cc10213393eace8de9
Alonsovn <alonsonh94@gmail.com>
2023-04-21 16:38:20 -0600

    add objects practice

---
+++ practice/practice_object_types.ts
@@ -0,0 +1,75 @@
+(() => {
+  type Vehiculo = {
+    carroceria: string;
+    modelo: string;
+    antibalas: boolean;
+    pasajeros: number;
+    disparar?: () => void;
+  };
+
+  let batimovil: Vehiculo = {
+    carroceria: "Negra",
+    modelo: "6x6",
+    antibalas: true,
+    pasajeros: 4,
+  };
+
+  let bumblebee: Vehiculo = {
+    carroceria: "Amarillo con Negro",
+    modelo: "4x2",
+    antibalas: true,
+    pasajeros: 4,
+    disparar() {
+      console.log("Disparando");
+    },
+  };
+
+  type Villano = {
+    nombre: string;
+    edad: number | undefined;
+    mutante: boolean;
+  };
+
+  let villanos: Villano[] = [
+    {
+      nombre: "Lex Luthor",
+      edad: 52,
+      mutante: false,
+    },
+    {
+      nombre: "Erick Magnus Lehnsherr",
+      edad: 49,
+      mutante: true,
+    },
+    {
+      nombre: "James Logan",
+      edad: undefined,
+      mutante: true,
+    },
+  ];
+
+  type Charles = {
+    poder: string;
+    estatura: number;
+  };
+
+  const charles: Charles = {
+    poder: "psiquico",
+    estatura: 1.78,
+  };
+
+  type Apocalipsis = {
+    lider: boolean;
+    miembros: string[];
+  };
+
+  const apocalipsis: Apocalipsis = {
+    lider: true,
+    miembros: ["Magneto", "Tormenta", "Angel"],
+  };
+
+  let mystique: Charles | Apocalipsis;
+
+  mystique = charles;
+  mystique = apocalipsis;
+})();
